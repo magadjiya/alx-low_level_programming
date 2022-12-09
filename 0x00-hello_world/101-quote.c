d3ebd56dd5b56f9f72334b82d655986e016c067a
magadjiya <khadijaumardh@gmail.com>
2022-12-08 20:03:41 -0800

    write wins the day

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,8 +9,8 @@
 */
 int main(void)
 {
-	int c = stderr;
+	char s[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	fwrite(&c, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(1, s, 59);
 	return (1);
 }

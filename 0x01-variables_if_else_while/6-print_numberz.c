aa5164bf5543a21baea6faa42a29c821456dbff1
magadjiya <khadijaumardh@gmail.com>
2022-12-09 20:59:13 -0800

    +0

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * putchar 0-9
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
+	return (0);
+}

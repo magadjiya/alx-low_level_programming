1fbff048847aa86dfc433426d86bc6357cded993
magadjiya <khadijaumardh@gmail.com>
2022-12-09 18:41:38 -0800

    minor fix for semicolons

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,15 +17,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("%i is positive\n", n)
+		printf("%i is positive\n", n);
 	}
 	else if (n == 0)
 	{
-		printf("%i is 0\n", n)
+		printf("%i is 0\n", n);
 	}
 	else
 	{
-		printf("%i is negative\n", n)
+		printf("%i is negative\n", n);
 	}
 	return (0);
 }

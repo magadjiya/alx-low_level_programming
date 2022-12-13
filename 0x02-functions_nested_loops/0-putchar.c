38d0f26c793c8c2645fedd7102e84c55df509e67
magadjiya <khadijaumardh@gmail.com>
2022-12-13 13:20:23 -0800

    wrote header file and called function inside it

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,23 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
  * main - Entry point
  *
- * print to std error
+ * 
  *
  * Return: 0
 */
 int main(void)
 {
-	char put[] = "__putchar\n";
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-	write(0, put, 10);
 	return (0);
 }

5427f3b0f8d4d7b80f3d0b431c78b83a6f00d88b
magadjiya <khadijaumardh@gmail.com>
2022-12-12 18:19:28 -0800

    break down ths problem

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * chevk a couple ifs
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	int n;
+	int l;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	l = n % 10;
+
+	if (l > 5)
+	{
+		printf("Last digit of %i is %i and is greater than 5\n", n, l);
+	}
+	else if (l == 0)
+	{
+		printf("Last digit of %i is %i and is 0\n", n, l);
+	}
+	else if (l < 6 && l != 0)
+	{
+		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, l);
+	}
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * putchar hexadeximal
+ *
+ * Rerurn: 0
+*/
+int main(void)
+{
+	int n;
+	char l;
+
+	for (n = 0; n  <= 9; n++)
+	{
+		putchar((n % 10) + '0');
+	}
+	for (l = 'a'; l <= 'f'; l++)
+	{
+		putchar(l);
+	}
+	putchar('\n');
+	return (0);
+}

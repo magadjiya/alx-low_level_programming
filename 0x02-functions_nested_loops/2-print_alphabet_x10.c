20a27907b7e3f3f7e75a7f517037af40c50a2141
magadjiya <khadijaumardh@gmail.com>
2022-12-13 18:00:27 -0800

    push all

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Return: void
+*/
+int print_alphabet_x10(void)
+{
+	int i = 0;
+	char letter;
+
+	while (i < 10)
+	{
+		letter = 'a';
+		
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}

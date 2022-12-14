891fa97a93f0df99496ad1b24eb4f87287ebde93
magadjiya <khadijaumardh@gmail.com>
2022-12-13 17:54:37 -0800

    removed main

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,22 +1,6 @@
 #include "main.h"
 
 /**
- * main - entry point
- *
- * print_alphabet - called from
- *
- * Return: 0
-*/
-
-void print_alphabet(void);
-
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
-/**
  * print_alphabet - entry
  *
  * _putchar from a to z

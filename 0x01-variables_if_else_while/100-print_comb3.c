7ec8650e538abe7df932c0e0575597de21f1eca9
magadjiya <khadijaumardh@gmail.com>
2022-12-15 05:54:30 -0800

    print number combos

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * print all combinations of two digit numbers
+ *
+ * 01 and 10 are considered the same, should be excluded
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	int f = 0, s;
+
+	while (f <= 9)
+	{
+		for (f, s = 1; f <= 9, s <= 9; s++)
+		{
+			putchar(f + '0');
+			putchar(s + '0');
+			putchar(44);
+		}
+		f++;
+	}
+	putchar('\n');
+	return (0);
+}

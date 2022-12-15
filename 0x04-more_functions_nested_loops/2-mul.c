0c6c81ad8196f0102f1093b901d288dbd07d0446
magadjiya <khadijaumardh@gmail.com>
2022-12-15 12:18:23 -0800

    updated header and multiplucation program

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - multiply two numbers
+ *
+ * @a: first n
+ *
+ * @b: second n
+ *
+ * Return: 0
+*/
+int mul(int a, int b)
+{
+	int product = a * b;
+
+	_putchar(product);
+	return (0);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,5 +2,7 @@
  * all prototypes
 */
 
+int _putchar(int character);
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);

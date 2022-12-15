b5657e427cefbac31bd03dafa77811f1508aa4c7
magadjiya <khadijaumardh@gmail.com>
2022-12-14 18:30:07 -0800

    if a bool is met, return 1

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdbool.h>
+
+/**
+ * _islower - entry point
+ *
+ * c - character
+ *
+ * Return: 1 if _islower == 'true', 0 if not
+*/
+int _islower(int c)
+{
+	if (_islower(c) == true)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}

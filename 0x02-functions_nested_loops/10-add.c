ddbdbaf878c30a3921345708b406bae4c1772ecf
magadjiya <khadijaumardh@gmail.com>
2022-12-18 05:54:59 -0800

    add two ints together

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * add - function that adds two numbers together
+ *
+ * @f: first integer to be added
+ *
+ * @s: second integer to be added
+ *
+ * Return: 0
+*/
+int add(int f, int s)
+{
+	int sum = 0;
+
+	if (sum == 0)
+	{
+		sum = f + s;
+	}
+	return (sum);
+}

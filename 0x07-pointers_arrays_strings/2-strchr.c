454aae2fdd96bdc1b387d4989f55e4aa31629309
magadjiya <khadijaumardh@gmail.com>
2023-01-02 19:26:09 -0800

    derefernce, and Null

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - checks for a character in string
+ * @s: string to be checked
+ * @c: character to check for
+ * Return: pointer to c, NULL if not found
+*/
+char *_strchr(char *s, char c)
+{
+	/*dereference s to loop through it*/
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (NULL);
+}

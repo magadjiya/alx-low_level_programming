d6d8fd5c43f707b304718b5379f8cc0f6d20fc1c
magadjiya <khadijaumardh@gmail.com>
2023-01-04 15:19:37 -0800

    slight fix

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,13 +1,15 @@
 #include "main.h"
 
 /**
+ * factorial - function that returns the factorial of a number
+ * @n: integer inputted
+ * Return: factorial of n
  */
 int factorial(int n)
 {
 	if (n < 0)
 		return (-1);
 	else if (n == 0)
-		return (1);h
-	else
-		return (n * factorial(n -1));
+		return (1);
+	return (n * factorial(n - 1));
 }
--- 0x08-recursion/6-is_prime_number.c
+++
@@ -1,8 +0,0 @@
-#include "main.h"
-
-/**
-*/
-int is_prime_number(int n)
-{
-	//return 1 if prime
-}

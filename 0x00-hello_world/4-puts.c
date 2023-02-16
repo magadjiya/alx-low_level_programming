94e3b38eb6fcc9b59a60f9d9e84b547a47f3a18a
Khadija Danhassan <khadijaumardh@gmail.com>
2023-02-16 10:39:30 -0800

    print a sentence with puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - print a sentence with puts()
+ * Return: 0
+ */
+int main(void)
+{
+	char chars[] = "Progranming is like building a multilingual puzzle";
+
+	puts(chars);
+	return (0);
+}

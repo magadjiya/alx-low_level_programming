7b37c1f06f852a9716da603de86562b6acc9582f
Khadija Danhassan <khadijaumardh@gmail.com>
2023-02-16 15:32:27 -0800

    to stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print to stderr
+ * Return: 1
+ */
+int main(void)
+{
+	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	fwrite(msg, 59, 2, stderr);
+	return (1);
+}

720f90e3b4062f71c0f5234d12f39c2ced37986a
Khadija Danhassan <khadijaumardh@gmail.com>
2023-02-17 06:41:51 -0800

    positive ot negative random number

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - check if random number is +, - or 0
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n == 0)
+		printf("%d is zero\n", n);
+	else if (n > 0)
+		printf("%d is positive\n", n);
+	else
+		printf("%d is negative\n", n);
+	return (0);
+}

7fe59976ba2ffa68a5b0bdba39cccbbb3f9daeb3
Khadija Danhassan <khadijaumardh@gmail.com>
2023-02-20 14:43:03 -0800

    i dont know why but it works

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - print all combos of two 2-digit numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int a, b, c, d;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 9; b++)
+		{
+			for (c = a; c < 10; c++)
+			{
+				d = b + 1;
+				while (d <= 9)
+				{
+					putchar(a + 48);
+					putchar(b + 48);
+					putchar(' ');
+					putchar(c + 48);
+					putchar(d + 48);
+					if (b < 9)
+					{
+						putchar(44);
+						putchar(32);
+					}
+					d++;
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

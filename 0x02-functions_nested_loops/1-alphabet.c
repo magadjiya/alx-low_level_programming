3eb7309e089abf2a737ccfb224acb25de39431ca
Khadija Danhassan <khadijaumardh@gmail.com>
2023-02-21 05:01:34 -0800

    from a to z

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - prints a - z
+ */
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+}

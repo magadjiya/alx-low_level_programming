80f4baf4effbcaec4e969edf0a61f2e7983ad70d
Khadija Danhassan <khadijaumardh@gmail.com>
2023-02-22 15:09:34 -0800

    error fixed!

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,7 +14,6 @@ void print_to_98(int n)
 		{
 			printf("%d, ", n);
 		}
-		_putchar('\n');
 	}
 	else if (n < i)
 	{
@@ -22,7 +21,6 @@ void print_to_98(int n)
 		{
 			printf("%d, ", n);
 		}
-		_putchar('\n');
 	}
 	if (n == 98)
 		printf("%d\n", n);

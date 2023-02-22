214c32b3bef40638d6bb33c21b85d197f0870892
Khadija Danhassan <khadijaumardh@gmail.com>
2023-02-21 17:16:38 -0800

    50 bonacci!

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - print first 50 fibonacci numbers
+ * Return: 0
+ */
+int main(void)
+{
+	unsigned int i, a, b, c;
+
+	a = 1;
+	b = 2;
+	for (i = 0; i < 50; i++)
+	{
+		printf("%u", a);
+		c = a + b;
+		a = b;
+		b = c;
+		if (i < 49)
+			printf(", ");
+	}
+	printf("\n");
+	return (0);
+}

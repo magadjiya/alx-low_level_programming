e15fe7fc19efc7c8ce19e84a368b82072aedd844
Khadija Danhassan <khadijaumardh@gmail.com>
2023-02-23 02:54:04 -0800

    0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - print 0-9 followed by a new line
+ */
+void print_numbers(void)
+{
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+		_putchar(i + '0');
+		i++;
+	}
+	_putchar('\n');
+}

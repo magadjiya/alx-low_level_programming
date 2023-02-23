34d733ce6dafa57750286d4b57ef88fcc99096a3
Khadija Danhassan <khadijaumardh@gmail.com>
2023-02-23 08:28:51 -0800

    the fizzbuzz test!

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - fizzzz buzzzz
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 1; num <= 100; num++)
+	{
+		if (num % 3 == 0 && num % 5 == 0)
+			printf("Fizzbuzz");
+		else if (num % 3 == 0)
+			printf("Fizz");
+		else if (num % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", num);
+		if (num != 100)
+			putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}

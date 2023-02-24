b5718b858b0f12421f5278a829bdc3f782984806
Khadija Danhassan <khadijaumardh@gmail.com>
2023-02-24 14:31:24 -0800

    capital lettet B got me in a fit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,17 +10,15 @@ int main(void)
 	for (num = 1; num <= 100; num++)
 	{
 		if (num % 3 == 0 && num % 5 == 0)
-			printf("Fizzbuzz ");
+			printf("FizzBuzz");
 		else if (num % 3 == 0)
-			printf("Fizz ");
+			printf("Fizz");
 		else if (num % 5 == 0)
-			printf("Buzz ");
+			printf("Buzz");
 		else
 			printf("%d ", num);
-		/**
-		 * if (num != 100)
-		 * putchar(' ');
-		 */
+		if (num != 100)
+			putchar(' ');
 	}
 	putchar('\n');
 	return (0);

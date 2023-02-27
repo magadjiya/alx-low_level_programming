da0d2cd04355a2fce85a7fbe52e97ef47000ee49
Khadija Danhassan <khadijaumardh@gmail.com>
2023-02-27 11:08:42 -0800

    print for the world to see

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - print a sentence to stdout
+ * @str: given sentence
+ */
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}

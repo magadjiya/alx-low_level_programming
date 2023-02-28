001b098fbc2756eb4721f3406d536cede07aa63f
Khadija Danhassan <khadijaumardh@gmail.com>
2023-02-27 17:45:22 -0800

    betty and conditional fixed

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,12 +2,15 @@
 #include <stdio.h>
 
 /**
+ * print_array - print the elements of an array
+ * @a: pointer to the 1st element
+ * @n: length of array
  */
 void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n && n != 0; i++)
 	{
 		printf("%d", a[i]);
 		if (i < n - 1)

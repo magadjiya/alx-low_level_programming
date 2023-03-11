8d832ca51d75ce062212501ed549ad8c9efe66b3
Khadija Danhassan <khadijaumardh@gmail.com>
2023-03-10 20:42:02 -0800

    argument count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - print argument count
+ * @argc: argument count
+ * @argv: array of strings
+ * Return: 0
+ */
+int main(int argc, char **argv __attribute__ ((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}

73027c1a5d350db504e242dd531013db7e1428e9
Khadija Danhassan <khadijaumardh@gmail.com>
2023-03-20 09:27:36 -0700

    betty style documentation written

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
+#include <stdio.h>
+
+/**
+ * struct dog - contains the basic information about dogs
+ * @name: name of dog
+ * @age: the age of dog
+ * @owner: person who owns the dog
+ */
 struct dog
 {
 	char *name;

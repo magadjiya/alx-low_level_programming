718a070a48b2b7150f74301eb2ccf16a2baf99d5
Khadija Danhassan <khadijaumardh@gmail.com>
2023-03-20 18:51:02 -0700

    created typedef for struct dog, added remaining function declarations

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,8 +16,13 @@ struct dog
 	char *owner;
 };
 
-/* The other prototypes */
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
+typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif /* DOG_H */

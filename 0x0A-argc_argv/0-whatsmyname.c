b068f57a467b41e246efeafa406b9fe492e1da53
Khadija Danhassan <khadijaumardh@gmail.com>
2023-08-04 18:19:20 -0700

    readme and command line name function

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print name of program
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+int main(__attribute__((unused)) int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}

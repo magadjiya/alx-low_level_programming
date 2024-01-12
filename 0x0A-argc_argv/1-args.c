8565f88602e47dee40111c11d70a818572186298
Khadija Umar Danhassan <khadijaumardh@gmail.com>
2024-01-12 11:34:56 -0800

    print argument count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print number of cmd arguments
+ * @argc: argument count
+ * @argv: argument array
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	(void)argv;
+
+	printf("%d\n", argc);
+
+	return (0);
+}

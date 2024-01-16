81f527fb716e73bba80739c1059256a263a68c96
Khadija Umar Danhassan <khadijaumardh@gmail.com>
2024-01-16 13:52:56 -0800

    NULL as an empty string, cleaned up code a bit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -25,27 +25,31 @@ int _strlen(char *s)
 char *str_concat(char *s1, char *s2)
 {
 	char *ptr;
-	int i, j, length, len_s1;
+	int i, j, length;
 
-	length = _strlen(s1) + _strlen(s2);
-	len_s1 = _strlen(s1);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	length = _strlen(s1) + _strlen(s2) + 1;
 	ptr = malloc(sizeof(char) * length + 1);
 
 	if (ptr != NULL)
 	{
 		i = 0;
-		while (s1[i] != '\0')
+		while (i < _strlen(s1))
 		{
 			ptr[i] = s1[i];
 			i++;
 		}
 
 		j = 0;
-		while (s2[j] != '\0')
+		while (j < _strlen(s2))
 		{
 			/* add j to the length of the other string */
 			/* this ensures it always gives the required space */
-			ptr[len_s1 + j] = s2[j];
+			ptr[_strlen(s1) + j] = s2[j];
 			j++;
 		}
 		ptr[length] = '\0';

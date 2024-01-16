0d46e6c95177ecdf78c04a138d0d54935a6529aa
Khadija Umar Danhassan <khadijaumardh@gmail.com>
2024-01-16 13:55:26 -0800

    removed xtra space

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -32,7 +32,7 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		s2 = "";
 
-	length = _strlen(s1) + _strlen(s2) + 1;
+	length = _strlen(s1) + _strlen(s2);
 	ptr = malloc(sizeof(char) * length + 1);
 
 	if (ptr != NULL)

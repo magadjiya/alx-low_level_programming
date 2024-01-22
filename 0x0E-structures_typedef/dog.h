14625736b99c71cec9ef99daa82381fe095f8fda
Khadija Umar Danhassan <khadijaumardh@gmail.com>
2024-01-22 10:34:28 -0800

    basic structure for a dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog struct with basic info
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Description: grouped data for a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */

f087e1d6ff50b59d1c303215f24a815535795552
Khadija Umar Danhassan <khadijaumardh@gmail.com>
2024-01-22 07:06:48 -0800

    pi macro, abs macro, function that prints file name it was compiled from, i.e a macro

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,7 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+#define ABS(x) ((x) < (0) ? ((x) * -1) : (x))
+/*if x < 0, muliply by -1, else return x */
+
+#endif /* FUNCTION_LIKE_MACRO_H */

29e637337f95717a70dda79283d0a41cc17be634
Khadija Umar Danhassan <khadijaumardh@gmail.com>
2024-01-25 11:35:05 -0800

    complete header file, 2nd task: print integers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,35 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_numbers - variadic function to print numbers
+ * @separator: what we print btw the numbers
+ * print nothing of NULL
+ * @n: the number of integers
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	int i;
+	va_list args;
+
+	va_start(args, n);
+
+	i = 0;
+	while (i < (int)n)
+	{
+		if (i < (int)n - 1)
+		{
+			if (separator == NULL)
+				printf("%d", va_arg(args, int));
+			else
+				printf("%d%s", va_arg(args, int), separator);
+		}
+		else
+			printf("%d\n", va_arg(args, int));
+		i++;
+	}
+
+	va_end(args);
+}

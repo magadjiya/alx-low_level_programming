1fef5019c572a83277da048d392d0339bfbbef4c
Khadija Umar Danhassan <khadijaumardh@gmail.com>
2024-03-10 05:48:00 -0700

    header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,6 +3,7 @@
 
 /* Header files */
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * struct dog - a dog struct with basic info
@@ -28,8 +29,6 @@ void print_dog(struct dog *d);
 typedef struct dog dog_t;
 
 dog_t *new_dog(char *name, float age, char *owner);
-char *_strdup(const char *s);
-int _strlen(const char *s);
 void free_dog(dog_t *d);
 
 #endif /* DOG_H */
